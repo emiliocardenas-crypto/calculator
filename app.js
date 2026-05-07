e2a0b58f5ec07b006b44dbfee02c8d398b107ffd
emiliocardenas-crypto <emilio.cardenas@codedbykids.org>
2026-05-07 00:26:40 +0000

    the

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ function times() {
     const sum=(num1*num2)
     console.log(sum)
     document.getElementById("answer").textContent=sum
-}function divied() {
+}
+function divied() {
     const num1= Number(document.getElementById("num1").value)
     console.log(num1)
     const num2= Number(document.getElementById("num2").value)
@@ -32,4 +33,12 @@ function times() {
     const sum=(num1/num2)
     console.log(sum)
     document.getElementById("answer").textContent=sum
+}
+function squaredroot() {
+    const num3= Number(document.getElementById("num3").value)
+    console.log(num3)
+    let sum2 = Math.sqrt(num3); 
+    console.log(sum2); 
+    console.log(sum2)
+    document.getElementById("answer2").textContent=sum2
 }
